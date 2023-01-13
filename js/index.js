af61125000ecc7580d05fab3cacee448998aaa0d
Jarod Smith <lbriones.dev@gmail.com>
2023-01-13 12:24:31 -0300

    addProduct.html + viewProduct.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $("#table_product").DataTable();
+
+    $(".recomendar").on("click", function(){
+    if ($(this).hasClass("fa-regular")){
+        $(this).removeClass("fa-regular")
+        $(this).addClass("fa-solid")
+    }else{
+        $(this).removeClass("fa-solid")
+        $(this).addClass("fa-regular")
+    }
+})
 });
+
